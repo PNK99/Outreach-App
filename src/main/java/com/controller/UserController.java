205c4f1eefdcebc998fd4eac68ac4830b00e77a9
Hurair <839821@cognizant.com>
2020-03-05 15:19:38 +0530

    logout done

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bean.User;
