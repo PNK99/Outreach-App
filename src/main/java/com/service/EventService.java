5fac470bf7e2058dd8320500bfd40a32e1956d42
839821 <839821@PC182381.cts.com>
2020-03-06 12:59:23 +0530

    added test case related codes for testing event service

--- src/main/java/com/service/EventService.java
+++ src/main/java/com/service/EventService.java
@@ -21,57 +21,119 @@ public class EventService {
 	@Autowired
 	private EventDao eventDao;
 
-	public void subscribeEvent(int eventId, int userId) {
-		Optional<User> userOp = userDao.findById(userId);
-		Optional<Event> eventOp = eventDao.findById(eventId);
-
-		User user = userOp.get();
-		Event event = eventOp.get();
-
-		event.getVolunteers().add(user);
-
-		eventDao.save(event);
-
+	public UserDao getUserDao() {
+		return userDao;
 	}
 
-	public Set<Event> getYourEvents(int userId) {
-		Optional<User> user = userDao.findById(userId);
-		Set<Event> listOfEvents = user.get().getEvents();
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
 
+	public EventDao getEventDao() {
+		return eventDao;
+	}
+
+	public void setEventDao(EventDao eventDao) {
+		this.eventDao = eventDao;
+	}
+
+	public boolean subscribeEvent(Integer eventId, Integer userId) {
+
+		boolean result = true;
+
+		try {
+			Optional<User> userOp = userDao.findById(userId);
+			Optional<Event> eventOp = eventDao.findById(eventId);
+
+			User user = userOp.get();
+			Event event = eventOp.get();
+
+			event.getVolunteers().add(user);
+
+			eventDao.save(event);
+			
+		} catch (Exception e) {
+			result = false;
+		}
+
+		return result;
+	}
+
+	public Set<Event> getYourEvents(Integer userId) {
+
+		Set<Event> listOfEvents = null;
+
+		if (userId != null) {
+			Optional<User> user = userDao.findById(userId);
+			listOfEvents = user.get().getEvents();
+		}
+		
 		return listOfEvents;
 
 	}
 
-	public void unsubscribeEvent(int eventId, int userId) {
-		Optional<User> userOp = userDao.findById(userId);
-		Optional<Event> eventOp = eventDao.findById(eventId);
+	public boolean unsubscribeEvent(Integer eventId, Integer userId) {
 
-		User user = userOp.get();
-		Event event = eventOp.get();
+		boolean result = true;
 
-		event.getVolunteers().remove(user);
-		eventDao.save(event);
+		try {
+
+			Optional<User> userOp = userDao.findById(userId);
+			Optional<Event> eventOp = eventDao.findById(eventId);
+
+			User user = userOp.get();
+			Event event = eventOp.get();
+
+			event.getVolunteers().remove(user);
+			eventDao.save(event);
+		} catch (Exception e) {
+			result = false;
+		}
+
+		return result;
 
 	}
 
 	public List<User> getAllVolunteers(Integer userId) {
-		List<User> users = userDao.findAllVolunteers(userId);
+		List<User> users = null;
+
+		if (userId != null)
+			users = userDao.findAllVolunteers(userId);
+
 		return users;
 	}
-	
-	public void inviteVolunteer(int eventId, Integer ids[]) {
-		Event event = eventDao.findById(eventId).get();
 
-		for(Integer id: ids) {
-			User user =  userDao.findById(id).get();
-			event.getInvitedPeople().add(user);
+	public boolean inviteVolunteer(int eventId, Integer ids[]) {
+
+		boolean result = true;
+
+		try {
+			Event event = eventDao.findById(eventId).get();
+
+			for (Integer id : ids) {
+				User user = userDao.findById(id).get();
+				event.getInvitedPeople().add(user);
+			}
+			eventDao.save(event);
+
+		} catch (Exception e) {
+			result = false;
 		}
-		eventDao.save(event);
+
+		return result;
+
 	}
-	
-	public Set<Event> getInvitedEvents(int userId){
-		User user = userDao.findById(userId).get();
-		Set<Event> events = user.getInvitedEvents();
+
+	public Set<Event> getInvitedEvents(Integer userId) {
+
+		Set<Event> events = null;
+
+		if (userId != null) {
+
+			User user = userDao.findById(userId).get();
+			events = user.getInvitedEvents();
+
+		}
 		return events;
 	}
 
